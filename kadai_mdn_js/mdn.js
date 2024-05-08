e60093130f7c6ca3ac3b19d7e0b7fcde7a6287b0
samu-koi305 <167172059+samu-koi305@users.noreply.github.com>
2024-05-08 09:55:38 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,4 @@
 const date = new Date();
+const month = date.getMonth() + 1;
 
-console.log(date.getFullYear() + '年' + date.getMonth() + '月'  + date.getDate() + '日');
+console.log(date.getFullYear() + '年' + month + '月'  + date.getDate() + '日');
